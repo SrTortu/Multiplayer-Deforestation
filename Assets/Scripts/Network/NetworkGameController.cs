5d04a2f48b58f30bfd1d0b39a5f8088263db0ec2
DESKTOP-VMTN4KT\SrTortuga <santiagogc0828@gmail.com>
2025-09-09 11:20:38 +0200

    Initial comit

---
+++ Assets/Scripts/Network/NetworkGameController.cs
@@ -0,0 +1,44 @@
+using Deforestation.Interaction;
+using Deforestation.Machine;
+using Deforestation.Recolectables;
+using Photon.Pun;
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Deforestation.Network
+{
+
+	public class NetworkGameController : GameController
+	{
+
+		#region Public Methods
+		public void InitializePlayer (HealthSystem health, CharacterController player, Inventory inventory, InteractionSystem interaction, Transform playerFollow)
+		{
+			_playerHealth = health;
+			_player = player;
+			_inventory = inventory;
+			_interactionSystem = interaction;
+			_playerFollow = playerFollow;
+		}
+
+		public void InitializeMachine( Transform follow, MachineController machine)
+		{
+			if (_machine != null)
+			{
+				_machine.HealthSystem.OnHealthChanged -= _uiController.UpdateMachineHealth;
+			}
+
+			_machineFollow = follow;
+			_machine = machine;
+
+			_machine.HealthSystem.OnHealthChanged += _uiController.UpdateMachineHealth;
+			//Para refrescar la UI
+			_machine.HealthSystem.TakeDamage(0);
+		}
+
+		#endregion
+
+	}
+
+}
